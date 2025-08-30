ee99531d77cdb5dafbbb7dc34498ea8b161aeee9
Vaibhav Goyal <23f1000082@ds.study.iitm.ac.in>
2025-08-30 23:46:28 +0530

    regex issue

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -26,7 +26,7 @@ app.use(cors(corsOptions))
 
 app.use(express.static(path.join(__dirname, "../../Frontend/dist")));
 
-app.get("*", (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, "../../Frontend/dist/index.html"));
 });
 
