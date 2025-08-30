0936f824e27b4926db9bbb6582330dd1a7a010b0
Vaibhav Goyal <23f1000082@ds.study.iitm.ac.in>
2025-08-30 23:38:50 +0530

    imported express

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -5,6 +5,7 @@ import { app } from './app.js'
 import {dbConnect} from "./db/db.js"
 import path from 'path';
 import { fileURLToPath } from 'url';
+import express from "express";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
